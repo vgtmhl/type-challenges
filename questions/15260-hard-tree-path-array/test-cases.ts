f0275788ddf2438314a86e72caa801c53536564c
Neil Richter <19751938+noook@users.noreply.github.com>
2022-08-30 10:20:20 +0000

    feat(question): add #15260 - Tree path array

---
+++ questions/15260-hard-tree-path-array/test-cases.ts
@@ -0,0 +1,8 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<typeof example['foo']['bar'], ['a']>>,
+  Expect<Equal<typeof example['foo']['baz'], ['b'] | ['c']>>,
+  Expect<Equal<typeof example['foo'], ['bar'] | ['baz'] | ['bar', 'a'] | ['baz', 'b'] | ['baz', 'c']>>,
+]
