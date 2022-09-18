d9a87f9b46c005a217e7de2e2ac72c69cdaedc22
前端子鱼 <119984+mwc@users.noreply.github.com>
2022-09-18 12:43:51 +0000

    feat(question): add #16259 - 将类型为字面类型（标签类型）的属性，转换为基本类型。

---
+++ questions/16259-medium-jiang-lei-xing-wei-zi-mian-lei-xing-biao-qian-lei-xing-de-shu-xing-zhuan-huan-wei-ji-ben-lei-xing/test-cases.ts
@@ -0,0 +1,25 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type PersonInfo = {
+  name: 'Tom',
+  age: 30,
+  married: false,
+  addr: {
+    home: '123456',
+    phone: '13111111111'
+  }
+}
+
+type ExpectedResult = {
+  name: string,
+  age: number,
+  married: boolean,
+  addr: {
+    home: string,
+    phone: string
+  }
+}
+
+type cases = [
+  Expect<Equal<ToPrimitive<PersonInfo>, ExpectedResult>>
+]
